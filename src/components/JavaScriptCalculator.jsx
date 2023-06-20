3a86ef72fb80c9a4d2099d05b810c59af40143dd
Thai-Cong-Dang-010396 <congthai1996@gmail.com>
2023-06-20 22:37:50 +0700

    Fix JavaScriptCalculator version 2

--- src/components/JavaScriptCalculator.jsx
+++ src/components/JavaScriptCalculator.jsx
@@ -1,154 +1,185 @@
-import React, {useState, useEffect} from 'react'
-
+import React, {useState} from 'react'
 import '../css/JavaScriptCalculator.css'
 
 const JavaScriptCalculator = () => {
-  const [display, setDisplay] = useState('')
-  const [valuePress, setValuePress] = useState('0')
+  const operators = ["*", "/", "+", "-"];
+    const [exp, setExp] = useState("");
+    const [io, setIO] = useState("0");
+    const [solved, setSolved] = useState(false);
 
-  const butCalculators = [
-    {
-      id: 'clear',
-      value: 'AC'
-    }, 
-    {
-      id: 'divide',
-      value: '/'
-    }, 
-    {
-      id: 'multiply',
-      value: '*'
-    },
-    {
-      id: 'seven',
-      value: '7'
-    },
-    {
-      id: 'eight',
-      value: '8'
-    },
-    {
-      id: 'nine',
-      value: '9'
-    },
-    {
-      id: 'subtract',
-      value: '-'
-    },
-    {
-      id: 'four',
-      value: '4'
-    },
-    {
-      id: 'five',
-      value: '5'
-    },
-    {
-      id: 'six',
-      value: '6'
-    },
-    {
-      id: 'add',
-      value: '+'
-    },
-    {
-      id: 'one',
-      value: '1'
-    },
-    {
-      id: 'two',
-      value: '2'
-    },
-    {
-      id: 'three',
-      value: '3'
-    },
-    {
-      id: 'zero',
-      value: '0'
-    },
-    {
-      id: 'decimal',
-      value: '.'
-    }, {
-      id: 'equals',
-      value: '='
-    }
-  ];
-  const arrOperator = ['+', '-',  '*', '/'];
-
-  const handleNumber = (event) => {
-    const number = event.target.value;
-    const indexLastOperator = display.toString().lastIndexOf(' ');
-    const lastTermNumber = (indexLastOperator === -1) ? display : display.toString().slice(indexLastOperator);
-
-    if(number === 'AC') {
-      setValuePress('0')
-      return setDisplay('')
-    };
-    
-    if(number === '.' && display.endsWith('.')) {
-      return;
-    } else if(number === '.' && lastTermNumber.includes('.')) {
-      return;
-    };
-
-    if(display === '0') {
-      setDisplay(number)
-    }else {
-      setDisplay(display + number)
-    }; 
-    setValuePress(number)
-
-  }
-
-  const handleOperator = (event) => {
-    const operator = event.target.value;
-    setValuePress(operator)
-    setDisplay(display + ' ' + operator + ' ');
-  }
-
-  const handleEqual = () => {
-    if(display.toString().includes('=')) {
-      setValuePress((preValuePress) => {
-        setDisplay(preValuePress)
-      })
-      return;
-    }
-    const resutls = eval(display);
-    setValuePress(resutls);
-    setDisplay(preDisplay => preDisplay + ' = ' + resutls)
-  }
-
-  return (
-    <div className='app-JavaScripCalculator'>
-      <div className='calculator'>
-        <div>
-          <div className='formulaScreen' style={{width: "240px", textAlign:"right"}}>
-            <div>{display}</div>
-          </div>
-          <div style={{width: "240px", textAlign:"right"}}>
-            <div id='display'>{valuePress}</div>
-          </div>
-        </div>
-        <div className='butCalculator'>
-          {butCalculators.map((butCalculator) => (
-            <button 
-              key={butCalculator.value}
-              id={butCalculator.id} 
-              value={butCalculator.value}
-              onClick={
-                (butCalculator.value === '=') ? handleEqual : (
-                  (arrOperator.indexOf(butCalculator.value) === -1) ? handleNumber: handleOperator)                 
-              }
-            >
-              {butCalculator.value}
-            </button>
-          ))}
+    return (
+      <div id='app-Calculator'>
+        <div class="calculator">
+            <div class="exp">{exp}</div>
+            <div id="display" class="io">
+                {io}
+            </div>
+            <div class="ac">
+                <button id="clear" onClick={clear}>
+                    AC
+                </button>
+            </div>
+            <div class="d">
+                <button id="divide" onClick={() => op("/")}>
+                    /
+                </button>
+            </div>
+            <div class="m">
+                <button id="multiply" onClick={() => op("*")}>
+                    X
+                </button>
+            </div>
+            <div class="n7">
+                <button id="seven" onClick={() => n("7")}>
+                    7
+                </button>
+            </div>
+            <div class="n8">
+                <button id="eight" onClick={() => n("8")}>
+                    8
+                </button>
+            </div>
+            <div class="n9">
+                <button id="nine" onClick={() => n("9")}>
+                    9
+                </button>
+            </div>
+            <div class="s">
+                <button id="subtract" onClick={() => op("-")}>
+                    -
+                </button>
+            </div>
+            <div class="n4">
+                <button id="four" onClick={() => n("4")}>
+                    4
+                </button>
+            </div>
+            <div class="n5">
+                <button id="five" onClick={() => n("5")}>
+                    5
+                </button>
+            </div>
+            <div class="n6">
+                <button id="six" onClick={() => n("6")}>
+                    6
+                </button>
+            </div>
+            <div class="a">
+                <button id="add" onClick={() => op("+")}>
+                    +
+                </button>
+            </div>
+            <div class="n1">
+                <button id="one" onClick={() => n("1")}>
+                    1
+                </button>
+            </div>
+            <div class="n2">
+                <button id="two" onClick={() => n("2")}>
+                    2
+                </button>
+            </div>
+            <div class="n3">
+                <button id="three" onClick={() => n("3")}>
+                    3
+                </button>
+            </div>
+            <div class="eq">
+                <button id="equals" onClick={solve}>
+                    =
+                </button>
+            </div>
+            <div class="n0">
+                <button id="zero" onClick={() => n("0")}>
+                    0
+                </button>
+            </div>
+            <div class="dec">
+                <button id="decimal" onClick={() => n(".")}>
+                    .
+                </button>
+            </div>
         </div>
       </div>
-    </div>
-  )
+    );
+
+    function clear() {
+        setExp("");
+        setIO("0");
+    }
+
+    function op(toDo) {
+        // working copy
+        let wcExp = exp;
+
+        if (solved) {
+            wcExp = io;
+            setSolved(false);
+        }
+
+        if (toDo !== "-") {
+            wcExp = wcExp.replace(/[*\/+-]+$/, "");
+        } else if (toDo === "-" && wcExp.endsWith("-")) {
+            return;
+        }
+
+        setExp(wcExp + toDo);
+        setIO(toDo);
+    }
+
+    function n(digit) {
+        // working copy
+        let wcExp = exp;
+        let wcIO = io;
+
+        if (digit === "0" && wcExp === "0") {
+            return;
+        }
+
+        if (solved) {
+            wcExp = "";
+            wcIO = "";
+            setSolved(false);
+        }
+
+        if (operators.includes(io)) {
+            wcIO = "";
+        }
+
+        if (wcIO === "0") {
+            wcIO = "";
+        }
+
+        if (digit === ".") {
+            if (wcIO.includes(".")) {
+                return;
+            }
+
+            if (wcIO === "") {
+                wcIO = "0";
+            }
+
+            if (wcExp === "" || /[*\/+-]$/.test(wcExp)) {
+                wcExp += "0";
+            }
+        }
+
+        setExp(wcExp + digit);
+        setIO(wcIO + digit);
+    }
+
+    function solve() {
+        // working copy
+        let wcExp = exp;
+
+        wcExp = wcExp.replace(/[*\/+-]+$/, "");
+
+        const answer = eval(wcExp).toString();
+
+        setExp(wcExp + "=" + answer);
+        setIO(answer);
+        setSolved(true);
+    }
 }
 
 export default JavaScriptCalculator
